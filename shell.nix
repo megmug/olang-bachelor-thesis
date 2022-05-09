751ae4cdabbbb2460664a8242f910ae6e5ab4f29
Daniel Maier <maier.dan@campus.lmu.de>
2022-05-09 20:44:35 +0200

    update nix.shell

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import ( builtins.fetchGit {
     # nixos-21.11 at 2022-05-09
     url = "https://github.com/nixos/nixpkgs/";
-    ref = "refs/heads/release-21.11";
+    ref = "release-21.11";
     rev = "cf33704649bb95f44814692048b6a1c74467958b";
   } ) {} }:
 pkgs.mkShell {
