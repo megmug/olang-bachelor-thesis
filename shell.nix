22cc3f49fb10951efc67bb5426cb56959ba0354c
Daniel Maier <maier.dan@campus.lmu.de>
2022-05-09 20:26:16 +0200

    update shell.nix to pin all versions

--- shell.nix
+++ shell.nix
@@ -1,5 +1,9 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.11.tar.gz") {} }:
-
+{ pkgs ? import ( builtins.fetchGit {
+    # nixos-21.11 at 2022-05-09
+    url = "https://github.com/nixos/nixpkgs/";
+    ref = "refs/heads/release-21.11";
+    rev = "cf33704649bb95f44814692048b6a1c74467958b";
+  } ) {} }:
 pkgs.mkShell {
     buildInputs = [
         	pkgs.ghc
