4c6e9ede884a16a4be9753d44db945e2bdb94c4a
Daniel Maier <maier.dan@campus.lmu.de>
2022-05-25 02:26:17 +0200

    update nix channel revision

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
     # nixos-21.11 at 2022-05-10
     url = "https://github.com/nixos/nixpkgs/";
     ref = "release-21.11";
-    rev = "aa2f845096f72dde4ad0c168eeec387cbd2eae04";
+    rev = "38a25ca6dd1aa84f356d5d7c4d5ba617cc43844a";
   } ) {} }:
 pkgs.mkShell {
     buildInputs = [
